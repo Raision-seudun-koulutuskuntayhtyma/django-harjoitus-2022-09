0d68b0967ddc9c6f3e9ff2e47ddc4f036510f23b
Tuomas Suutari <tuomas@nepnep.net>
2022-09-26 10:02:48 +0300

    Päivitä modeleita

--- varauskalenteri/models.py
+++ varauskalenteri/models.py
@@ -2,14 +2,15 @@ import datetime
 
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 
 
 class Tapahtuma(models.Model):
     otsikko = models.CharField(max_length=200)
-    kuvaus = models.TextField()
+    kuvaus = models.TextField(blank=True)
     alku = models.DateTimeField()
-    loppu = models.DateTimeField()
+    loppu = models.DateTimeField(null=True, blank=True)
     osallistujat = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         blank=True,
@@ -17,7 +18,14 @@ class Tapahtuma(models.Model):
     paikkoja = models.IntegerField()
 
     def __str__(self):
-        return f"{self.otsikko} ({self.alku}--{self.loppu})"
+        alku = timezone.localtime(self.alku)
+        # Ternary operator - kolmiosainen operaattori:
+        # A if EHTO else B
+        #   - Jos EHTO on tosi, niin tulos on A
+        #   - Jos EHTO on epätosi, niin tulos on B
+        loppu = timezone.localtime(self.loppu) if self.loppu else None
+        loppu_teksti = f"{loppu:%d.%m.%Y %H:%M}" if loppu else ""
+        return f"{self.otsikko} ({alku:%d.%m.%Y %H:%M} -- {loppu_teksti})"
 
     def kesto(self) -> datetime.timedelta:
         return self.loppu - self.alku
