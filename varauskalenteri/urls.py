886bad63b43e4acb78a99c5dd9951ddf571d97c3
Tuomas Suutari <tuomas@nepnep.net>
2022-09-26 13:25:26 +0300

    Url-muutoksia ja tapahtumalistaus-nimimuutos

--- varauskalenteri/urls.py
+++ varauskalenteri/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import tapahtumalistaus, varaa_tapahtuma
 
 urlpatterns = [
-    path('', tapahtumalistaus, name="etusivu"),
-    path('<int:id>/', varaa_tapahtuma),
+    path('', tapahtumalistaus, name="tapahtumalistaus"),
+    path('tapahtuma/<int:id>/', varaa_tapahtuma, name="varaus"),
 ]
